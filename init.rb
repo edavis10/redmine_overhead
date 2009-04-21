246f5a4826084b549ecab20839e69c786c6fc530
Eric Davis <edavis@littlestreamsoftware.com>
2009-04-21 09:15:01 -0700

    [#2281] Added plugin documentation.

--- init.rb
+++ init.rb
@@ -1,8 +1,13 @@
 require 'redmine'
 
 Redmine::Plugin.register :redmine_overhead do
-  name 'Redmine Overhead plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
-  version '0.0.1'
+  name 'Overhead plugin'
+  author 'Eric Davis'
+  description 'Overhead is a plugin that can be used to group Time Entry Activities into billable and overhead groups'
+  url 'https://projects.littlestreamsoftware.com/projects/redmine-overhead'
+  author_url 'http://www.littlestreamsoftware.com'
+  version '0.1.0'
+
+  requires_redmine :version_or_higher => '0.8.0'
+
 end
