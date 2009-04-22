ebd06b893b4de65067cc50a9bf7a4b9715a343d4
Eric Davis <edavis@littlestreamsoftware.com>
2009-04-22 10:30:58 -0700

    [#2280] Added documentation.

--- lib/overhead_timesheet_hook.rb
+++ lib/overhead_timesheet_hook.rb
@@ -1,14 +1,16 @@
 class OverheadTimesheetHook < Redmine::Hook::ViewListener
 
+  # Adds a table header "Billable" to the report results
   def plugin_timesheet_views_timesheet_group_header(context={})
     return content_tag(:th, l(:overhead_field_billable))
   end
 
+  # Adds a blank table cell to the issue grouping option
   def plugin_timesheet_views_timesheet_time_entry_sum(context={})
     return content_tag(:td, '')
   end
   
-
+  # Adds the gree checkbox image if the TimeEntry is billable
   def plugin_timesheet_views_timesheet_time_entry(context={})
     time_entry = context[:time_entry]
     if time_entry && time_entry.billable? 
