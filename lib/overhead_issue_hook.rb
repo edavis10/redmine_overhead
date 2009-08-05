d3f6ca543338c5773394033a1ce72b5902352392
Eric Davis <edavis@littlestreamsoftware.com>
2009-08-05 15:49:05 -0700

    [#2271] Refactored the inline HTML generation to a helper and render call.

--- lib/overhead_issue_hook.rb
+++ lib/overhead_issue_hook.rb
@@ -1,19 +1,10 @@
 class OverheadIssueHook < Redmine::Hook::ViewListener
   def view_issues_show_details_bottom(context={})
     if context[:project].module_enabled?('budget_module')
-      billable_label = content_tag(:td,
-                                   content_tag(:strong, l(:overhead_billable_time)+":"),
-                                   :class => 'billable-hours')
-      billable_spent = content_tag(:td, l_hours(context[:issue].billable_time_spent))      
-
-      overhead_label = content_tag(:td,
-                                   content_tag(:strong, l(:overhead_overhead_time)+":"),
-                                   :class => 'overhead-hours')
-      overhead_spent = content_tag(:td, l_hours(context[:issue].overhead_time_spent))      
-
-      return content_tag(:tr,
-                         billable_label + billable_spent + overhead_label + overhead_spent,
-                         :class => 'overhead-plugin')
+      return context[:controller].send(:render_to_string, {
+                                         :partial => 'issues/show_overhead',
+                                         :locals => {:issue => context[:issue]}
+                                       })
     else
       return ''
     end
